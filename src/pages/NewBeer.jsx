1b2fb271c4c1405336931ab4462992402b40173e
Nekane Ibarlucea <nknib90@gmail.com>
2022-03-04 18:24:36 +0100

    done

--- src/pages/NewBeer.jsx
+++ src/pages/NewBeer.jsx
@@ -1,23 +1,27 @@
 import Header from "../components/Header"
 import axios from "axios";
 import { useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 
 export default function NewBeer(){
     const [formData, setFormData] = useState({})
-    const params = useParams(); // same as req.params.apartmentId
     const navigate = useNavigate();
 
     function handleSubmit(event){
         event.preventDefault()
         axios
         .post(
-          `https://ih-beers-api2.herokuapp.com/beers/new`, 
+         "https://ih-beers-api2.herokuapp.com/beers/new", 
           formData
           )
-          .then(() => navigate("/"))
-          .catch((error) => navigate(`/`));
+          .then((response) => {
+                console.log(response)
+              navigate("/beers")
+            }
+            )
+          .catch((error) => navigate("/new-beer"));
       }
 
 
@@ -35,29 +39,28 @@ export default function NewBeer(){
 
         <div className="AddApartmentPage">
 
-
             <form onSubmit={handleSubmit}>
             <h2>New Beer</h2>
                 <label>Name</label>
-                <input type="text" name="title" onChange={handleChange}/>
+                <input type="text" name="name" value={formData.name} onChange={handleChange}/>
 
                 <label>Tagline </label>
-                <input type="text" name="tagline" onChange={handleChange}/>
+                <input type="text" name="tagline" value={formData.tagline} onChange={handleChange}/>
 
                 <label>Description </label>
-                <input type="text" name="description" onChange={handleChange}/>
+                <input type="text" name="description" value={formData.description} onChange={handleChange}/>
 
                 <label>First brewed</label>
-                <input type="text" name="first-brewed" onChange={handleChange}/>
+                <input type="text" name="first_brewed" value={formData.first_brewed} onChange={handleChange}/>
 
                 <label>Brewers tips</label>
-                <input type="text" name="brewers-tips" onChange={handleChange}/>
+                <input type="text" name="brewers_tips" value={formData.brewers_tips} onChange={handleChange}/>
 
                 <label>Attenuation level</label>
-                <input type="number" name="attenuation-level" onChange={handleChange}/>
+                <input type="number" name="attenuation_level" value={formData.attenuation_level} onChange={handleChange}/>
 
                 <label>Contributed by</label>
-                <input type="text" name="contributed-by" onChange={handleChange}/>
+                <input type="text" name="contributed_by" value={formData.contributed_by} onChange={handleChange}/>
 
                 <button type="submit">Create beer</button>
 
